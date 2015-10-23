53217984afabbd6c19ee85b3682b8b95c8afb069
Trevis Thomas <trevisthomas@gmail.com>
2015-10-23 10:18:23 -0500

    Initial Commit

---
+++ AlbumExperimentThree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlbumExperimentThree
+//
+//  Created by Trevis Thomas on 10/23/15.
+//  Copyright © 2015 Trevis Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
